831ea08fc7a105c2eb51b35da5766b5f17ec6fba
Maximus <46528266+SmellyB3lly@users.noreply.github.com>
2020-10-07 21:01:23 +0200

    Vi har lavet på front end UI og backend.
    Troubleshooter stadig hvordan vi kan få et user-generated cpr-nummer til at blive lavet om og tjekket af koden selv.
    
    Co-Authored-By: niko391a <69769473+niko391a@users.noreply.github.com>

--- CPR-nummer Opgave/Form1.cs
+++ CPR-nummer Opgave/Form1.cs
@@ -20,8 +20,54 @@ namespace CPR_nummer_Opgave
 
         private void buttonCPR_Click(object sender, EventArgs e)
         {
-            string pattern = "^[0-10]";
-            string cpr = tbCPR1.Text;
+            string pattern = "^[0-9]";
+
+            string cpr = tbCPR1.Text; 
+
+            string[] cprr = (string[])cpr.Clone();
+
+            cprr.Select(N1 => 0 * 4);
+
+            try
+            {
+                bool tst = Regex.IsMatch(cpr, pattern);
+
+                string N1 = cpr.Substring(0, 1);
+                string N2 = cpr.Substring(1, 1);
+                string N3 = cpr.Substring(2, 1);
+                string N4 = cpr.Substring(3, 1);
+                string N5 = cpr.Substring(4, 1);
+                string N6 = cpr.Substring(5, 1);
+                string N7 = cpr.Substring(6, 1);
+                string N8 = cpr.Substring(7, 1);
+                string N9 = cpr.Substring(8, 1);
+                string N10 = cpr.Substring(9, 1);
+
+
+                //string date = cpr.Substring(0, 2);
+                //string month = cpr.Substring(2, 2);
+                //string year = cpr.Substring(4, 2);
+
+
+                if ((string.Compare(cpr.Substring(6, 1), "-") == 0) & (cpr.Length == 11) & (cpr.Trim().Length == cpr.Length))
+                {
+                    tbValidCPR.Text = $"CPR-nummer {cpr} kan være gyldig.";
+
+                    //foreach (var item in cprrr)
+                    //{
+                    //    tbValidCPR.Text = (item);
+                    //}
+                }
+                else
+                {
+                    tbValidCPR.Text = $"CPR-nummer {cpr} kan være ugyldigt.";
+                }
+            }
+            catch (Exception)
+            {
+                tbValidCPR.Text = "Du har ikke indtastet korrekt.";
+            }
+            Console.ReadLine();
 
         }
     }
